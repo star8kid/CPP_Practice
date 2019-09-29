787ff9af620705f1ffce09aeb39ff2996f2294dd
BLUE 2 <BlUE2@ratpack.com>
2019-09-29 13:14:55 -0400

    Added Controllers

--- Practice/src/main/include/Robot.h
+++ Practice/src/main/include/Robot.h
@@ -8,7 +8,8 @@
 #pragma once
 
 #include <string>
-
+#include<ctre/Phownix.h>
+#include<frc/WPILib.h>
 #include <frc/IterativeRobot.h>
 #include <frc/smartdashboard/SendableChooser.h>
 
@@ -22,6 +23,27 @@ class Robot : public frc::IterativeRobot {
   void TeleopPeriodic() override;
   void TestPeriodic() override;
   double Absolute_Multiply(double first_num, double second_num);
+  //Motor IDs
+  static const int LEFT_FRONT = 1;
+  static const int LEFT_BACK = 2;
+  static const int RIGHT_FRONT = 3;
+  static const int RIGHT_BACK = 4;
+
+  VictorSPX fr_left{LEFT_FRONT};
+  VictorSPX fr_right{RIGHT_FRONT};
+  VictorSRX back_left{LEFT_BACK};
+  VictorSRX back_right{RIGHT_BACK};
+
+  frc::SpeedControllerGroup leftSide{fr_left, fr_right};
+  frc::SpeedControllerGroup rightSide{back_left, back_right};
+
+  frc::DifferentialDrive driveTrain{leftSide,rightSide};
+
+  frc:: XboxController pilot{0};
+
+
+  frc:: GenericHD :: JoyStickHand LEFT = frc::GenericHD :: kLeftHand;
+
  private:
 
 };
